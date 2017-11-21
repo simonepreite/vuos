fb4e958529bc6861d2acb2e3312815c3369a1b94
simonepreite <simone-preite_89@hotmail.it>
2017-11-21 18:38:20 +0100

    add my module to handle vnslib in vuos

--- test_modules/unrealvnslib.c
+++ test_modules/unrealvnslib.c
@@ -31,14 +31,7 @@
 #include <sys/epoll.h>
 #include <vumodule.h>
 #include <errno.h>
-
-#include <asm/types.h>
-#include <linux/netlink.h>
-#include <linux/rtnetlink.h>
-
-#include <arpa/inet.h>
-
-#include <string.h>
+#include "../../vnslib/vnslib.h"
 
 VU_PROTOTYPES(unrealvnslib)
 
@@ -79,53 +72,13 @@ int vu_unrealvnslib_msocket(const char *path, int domain, int type, int protocol
 static struct vuht_entry_t *ht[3];
 static int afs[3] = {AF_INET, AF_INET6, AF_NETLINK};
 
-struct nlmsghdr* hd;
-
 ssize_t vu_unrealvnslib_sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen){
 
-		struct nlmsghdr* header = hd = (struct nlmsghdr*) buf;
 		if(fd == sockfd){
-			//unsigned char* ip_string[sizeof(struct in6_addr)];
-			char str[INET6_ADDRSTRLEN];
-			if(header->nlmsg_type == RTM_NEWROUTE || header->nlmsg_type == RTM_DELROUTE || header->nlmsg_type == RTM_GETROUTE){
-				struct rtmsg* rtm = (struct rtmsg*)(NLMSG_DATA(header));
-				struct rtattr* attr = (struct rtattr*)((void*)(((char*)rtm)+ sizeof(struct rtmsg)));
-
-				if(rtm->rtm_family == AF_INET6){
-					struct in6_addr* ip6_a = (struct in6_addr*)RTA_DATA(attr);
-					inet_ntop(rtm->rtm_family, ip6_a, str, INET6_ADDRSTRLEN);
-					printk("IPv6 address route: %s\n",str);
-				}
-				else if(rtm->rtm_family == AF_INET){
-					struct in_addr* ip4_a = (struct in_addr*)RTA_DATA(attr);
-					printk("IPv4 address route: %s\n", inet_ntoa(*ip4_a));
-				}
-
-			}
-			else if(header->nlmsg_type == RTM_NEWADDR || header->nlmsg_type == RTM_DELADDR || header->nlmsg_type == RTM_GETADDR){
-				struct ifaddrmsg* ifa = (struct ifaddrmsg*)(NLMSG_DATA(header));
-				struct rtattr* attr = (struct rtattr*)((void*)(((char*)ifa)+ sizeof(struct ifaddrmsg)));
-				//struct in_addr* ip_a = (struct in_addr*)RTA_DATA(attr);
-				if(ifa->ifa_family == AF_INET6){
-					struct in6_addr* ip6_a = (struct in6_addr*)RTA_DATA(attr);
-					inet_ntop(ifa->ifa_family, ip6_a, str, INET6_ADDRSTRLEN);
-					printk("IPv6 address add: %s\n",str);
-					inet_pton(ifa->ifa_family, str, ip6_a);
-				}
-				else if(ifa->ifa_family == AF_INET){
-					struct in_addr* ip4_a = (struct in_addr*)RTA_DATA(attr);
-					printk("IPv4 address add: %s\n", inet_ntoa(*ip4_a));
-				}
-				//printk("prefix: %d\n", ifa->ifa_prefixlen);
-				//printk("ip_add: %s\n", inet_ntoa(*ip_a));
-			}
-			else if(header->nlmsg_type == RTM_NEWLINK || header->nlmsg_type == RTM_DELLINK || header->nlmsg_type == RTM_GETLINK){
-				//struct ifinfomsg* ifi = (struct ifinfomsg*)(NLMSG_DATA(header));
-				//struct rtattr* attr = (struct rtattr*)((void*)(((char*)ifi)+ sizeof(struct ifinfomsg)));
-			}
 			printk("sendto:%d\n", sockfd);
+			print_address(buf);
 			return sendto(sockfd, buf, len, flags,dest_addr, addrlen);
-	}
+		}
 	return sendto(sockfd, buf, len, flags,dest_addr, addrlen);
 }
 
